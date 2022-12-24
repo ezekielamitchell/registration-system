08f808bc9452a2063f805780f0211844b9b83bcf
Ezekiel A. Mitchell <ezekielem@outlook.com>
2022-12-24 13:03:19 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include<string>
+
+using namespace std;
+
+int menu() {
+    int input;
+
+    cout << "*----- Menu -----*" << endl;
+    cout << "1) Register" << endl;
+    cout << "2) Log In" << endl;
+    cout << "3) exit" << endl;
+    cout << "enter option: ";
+    cin >> input;
+
+    // invalid user input
+    if (std::cin.fail() || !(input == 1 || input == 2 || input == 3)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cerr << "Invalid input; please enter 1, 2, or 0" <<endl;
+    }
+
+    return input;
+}
+
+struct User {
+    string accountNumber;
+    string fName;
+    string lName;
+    string userName;
+    string password;
+};
+
+User registerUser() {
+    User user;
+
+    return user;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    bool active = true;
+    while (active) {
+        switch (menu()) {
+            case 1:
+                cout << 1 << endl;
+                break;
+            case 2:
+                cout << 2 << endl;
+                break;
+            case 3:
+                cout << "exiting systems...";
+                active = false;
+        }
+
+    }
     return 0;
 }
+
